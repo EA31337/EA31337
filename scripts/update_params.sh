d89267e60852b2ccb0e2ed4c12531c7c12093131
kenorb <kenorb@users.noreply.github.com>
2016-11-17 00:50:11 +0000

    Corrects mqh file path in update_params.sh

--- scripts/update_params.sh
+++ scripts/update_params.sh
@@ -2,4 +2,4 @@
 # Script to update parameters from set into mqh files.
 ROOT="$(git rev-parse --show-toplevel)"
 CWD=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-$CWD/convert_set_mqh.sh $ROOT/sets/Lite/EURUSD/default/2000USD/10-spread/5-digits/2014/EA31337-Lite.set $ROOT/src/include/EA/ea-input-lite.mqh 
+$CWD/convert_set_mqh.sh $ROOT/sets/Lite/EURUSD/default/2000USD/10-spread/5-digits/2014-2015/EA31337-Lite.set $ROOT/src/include/EA/lite/ea-input.mqh
