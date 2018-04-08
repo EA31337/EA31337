c97e8f7e9538bb2cf943fa3ae52d2c91a460811f
kenorb <kenorb@users.noreply.github.com>
2018-04-08 01:01:21 +0100

    Removes refs to PEM file

--- conf/aws/load_env.lite.inc.sh
+++ conf/aws/load_env.lite.inc.sh
@@ -1,12 +1,8 @@
 #!/bin/sh
 export PROVIDER=aws
 export INSTANCE_TYPE=t2.small
-export KEYPAIR_NAME="EA31337-Tester"
-export SUBNET_ID="subnet-2997c75e"
-export PRIVATE_KEY="$HOME/Projects/EA31337/EA31337/conf/certs/EA31337-Tester.pem"
-export ASSET="EA31337 EA31337-Backtest-Releases v1.069 Lite-Backtest ~/files/EA"
-export GITHUB_API_TOKEN=b9c04ee7804b9731d9f64b6826375625760dd960 
-export CLONE_REPO="https://9b3e1c0c4314ff16839b4382e13a0367a432de71@github.com/EA31337-Tester/EA31337-Lite-Sets"
+export ASSET="EA31337 EA31337 v1.076 Lite-Backtest ~/files/EA"
+export CLONE_REPO="https://github.com/EA31337-Tester/EA31337-Lite-Sets"
 export GIT_ARGS='--author="EA31337-Tester <EA31337-Tester@users.noreply.github.com>"'
 export TERMINATE=1
 export POWER_OFF=1
---
+++ conf/aws/load_env.local.inc.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export PROVIDER=aws
+export INSTANCE_TYPE=t2.small
+#export TERMINATE=1
+export POWER_OFF=1
+#export NO_SETUP=1
