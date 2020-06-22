7c3b635770be9301af55925c35c5cdbbbd37c811
kenorb <kenorb@users.noreply.github.com>
2020-06-22 23:34:39 +0100

    Fixes copy_set_mqh script

--- scripts/copy_set_mqh.sh
+++ scripts/copy_set_mqh.sh
@@ -8,13 +8,13 @@ on_error() { echo "Error!"; while caller $((n++)); do :; done >&2; }
 ROOT=$(git rev-parse --show-toplevel 2> /dev/null || (cd -P -- "$(dirname -- "$0")/.." && pwd -P))
 [ $# -lt 1 ] && { echo "Usage: $0 (lite/advanced/rider)"; exit 1; }
 read -r mode <<<"$@"
-MQHFILE="$ROOT/src/include/EA31337/$mode/ea-input.mqh"
-[ -f "$MQHFILE" ] || { echo "Invalid mode: $mode!"; exit 1; }
+[ -d "$ROOT/docker/optimization/$mode" ] || { echo "Invalid mode: $mode!"; exit 1; }
 echo "Copying..."
 while read -r opt; do
   name=${opt%=*}
   eval "$opt"
   value=${!name}
-  ex "+%s/ $name\\s\\+=.*;/ $name = $value;/" -scwq "$MQHFILE"
+  dest_file=$(grep -lwr "$name" "$ROOT"/src/include/EA31337/"$mode"/*.mqh "$ROOT"/src/include/EA31337-strategies/*/*.mqh)
+  ex "+%s/ $name\\s\\+=.*;/ $name = $value;/" -scwq "$dest_file"
 done < <(find "$ROOT/docker/optimization/$mode" -name "*.set" -exec grep -hA99 Optimization {} ';' | grep -o "^[A-Z][^=,]\\+=[0-9.]\\+")
 echo "done."
