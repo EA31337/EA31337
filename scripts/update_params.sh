c235791ed5b40ac2a77502c43eb6d5a5760e13cf
kenorb <kenorb@users.noreply.github.com>
2021-09-20 19:08:52 +0100

    Removes some old scripts

--- scripts/update_params.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Script to update parameters from set into mqh files.
-ROOT="$(git rev-parse --show-toplevel)"
-CWD=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
-"$CWD"/convert_set_mqh.sh "$ROOT"/sets/Lite/EURUSD/default/10000USD/20-spread/5-digits/2014-2016/EA31337-Lite.set "$ROOT"/src/include/EA31337/lite/ea-input.mqh
