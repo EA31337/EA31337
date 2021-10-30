6be74f30d6d21c23672f81cd23c63076f6eed07f
kenorb <kenorb@users.noreply.github.com>
2021-10-30 23:52:32 +0100

    Updates version to 2.009

--- src/include/common/define.h
+++ src/include/common/define.h
@@ -5,7 +5,7 @@
 //+------------------------------------------------------------------+
 
 // EA defines.
-#define ea_version "2.008"
+#define ea_version "2.009"
 #define ea_desc "Forex multi-strategy trading robot."
 #define ea_link "https://github.com/EA31337"
 #define ea_author "kenorb"
