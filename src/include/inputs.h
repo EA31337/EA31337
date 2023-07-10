59837b88a257c3b138370b1b161b23d1c4c9c37d
kenorb <kenorb@users.noreply.github.com>
2023-07-10 22:17:58 +0100

    Increases risk margin to 1.8%

--- src/include/inputs.h
+++ src/include/inputs.h
@@ -41,7 +41,7 @@ extern string __EA_Risk_Params__ = "-- EA's risk management --";  // >>> EA's RI
 #else
 input group "EA's risk management"
 #endif
-input float EA_Risk_MarginMax = 1.2f;  // Max margin to risk (in %)
+input float EA_Risk_MarginMax = 1.8f;  // Max margin to risk (in %)
 
 #ifdef __MQL4__
 input string __EA_Trade_Params__ = "-- EA's trade parameters --";  // >>> EA's TRADE <<<
