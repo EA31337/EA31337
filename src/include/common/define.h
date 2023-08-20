311d615982acda01a2b84aeb1c140d8e688b3695
kenorb <kenorb@users.noreply.github.com>
2023-08-20 14:20:23 +0100

    Updates indicators for compatibility with v3 release

--- src/include/common/define.h
+++ src/include/common/define.h
@@ -52,9 +52,9 @@
 
 // Indicator defines.
 #define INDI_ATR_MA_TREND_PATH "indicators\\Other\\Misc\\ATR_MA_Trend"
-#define INDI_EWO_OSC_PATH "indicators\\Other\\Oscillator\\Elliott_Wave_Oscillator2"
-#define INDI_SAWA_PATH "indicators\\Other\\PriceBands\\SAWA"
+#define INDI_EWO_OSC_PATH "indicators\\Other\\Oscillators\\Elliott_Wave_Oscillator2"
+#define INDI_SAWA_PATH "indicators\\Other\\Price\\Range\\SAWA"
 #define INDI_SUPERTREND_PATH "indicators\\Other\\Price\\SuperTrend"
-#define INDI_SVEBB_PATH "indicators\\Other\\Oscillator\\SVE_Bollinger_Bands"
-#define INDI_TMA_CG_PATH "indicators\\Other\\PriceBands\\TMA+CG_mladen_NRP"
-#define INDI_TMA_TRUE_PATH "indicators\\Other\\PriceBands\\TMA_True"
+#define INDI_SVEBB_PATH "indicators\\Other\\Oscillators\\SVE_Bollinger_Bands"
+#define INDI_TMA_CG_PATH "indicators\\Other\\Price\\Range\\TMA+CG_mladen_NRP"
+#define INDI_TMA_TRUE_PATH "indicators\\Other\\Price\\Range\\TMA_True"
