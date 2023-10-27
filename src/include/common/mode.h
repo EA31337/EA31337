32b4aa628b4236ffdddc41738aaa801dddbc7656
kenorb <kenorb@users.noreply.github.com>
2023-10-27 22:16:31 +0100

    Reformats mode.h

--- src/include/common/mode.h
+++ src/include/common/mode.h
@@ -3,25 +3,25 @@
 //|                                       https://ea31337.github.io/ |
 //+------------------------------------------------------------------+
 
-// Main modes (Lite, Advanced, Rider or Elite).
-// Uncomment the line to activate the version.
+/* Main modes (Lite, Advanced, Rider or Elite). */
+/* Uncomment selected line to activate the specific version. */
 // For Lite (default), leave below modes commented out.
-//#define __advanced__  // Enables configuration for Advanced.
-//#define __rider__  // Enables configuration for Rider.
-//#define __elite__  // Enables configuration for Elite.
+// #define __advanced__  // Enables configuration for Advanced.
+// #define __rider__  // Enables configuration for Rider.
+// #define __elite__  // Enables configuration for Elite.
 
-// Enabled modes by default.
-//#define __config__  // Loads params from the config files.
+/* Enabled modes by default. */
+// #define __config__  // Loads params from the config files.
 
-// Optional modes (Uncomment to enable).
-//#define __backtest__     // For backtest only.
-//#define __cli__          // Enables CLI mode.
-//#define __debug__        // Enables debugging.
-//#define __input__        // Enables user input params.
-//#define __limited__      // Defines safe options.
-//#define __optimize__     // Optimization mode.
-//#define __profiler__     // Activates profiler.
-//#define __property__     // Enables program properties.
-//#define __release__      // Enables release settings.
-#define __resource__     // Enables resources.
-//#define __trace__        // Enables tracing.
+/* Optional modes (Uncomment selected lines to enable specific mode). */
+// #define __backtest__     // For backtest only.
+// #define __cli__          // Enables CLI mode.
+// #define __debug__        // Enables debugging.
+// #define __input__        // Enables user input params.
+// #define __limited__      // Defines safe options.
+// #define __optimize__     // Optimization mode.
+// #define __profiler__     // Activates profiler.
+// #define __property__     // Enables program properties.
+// #define __release__      // Enables release settings.
+// #define __resource__     // Enables resources.
+// #define __trace__        // Enables tracing.
